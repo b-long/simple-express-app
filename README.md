83c9db9b9e0c4ca010fb585c7e7a6d3f3e2cb126
b-long <(none)>
2017-08-17 23:50:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Express App
+
+A bare bones NodeJS / Express application, for demonstration purposes.
+
+To run this module locally:
+```shell
+PORT="8080" node hello.js
+```
+
+[![Build Status](https://travis-ci.org/b-long/simple-express-app.svg)](https://travis-ci.org/b-long/simple-express-app)
---
+++ hello.js
@@ -0,0 +1,22 @@
+const os = require("os");
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World! <br/>' + 
+    'Welcome to a NodeJS application, deploybed by Ansible <br/>' +
+    '<img src="./node-logo.png" width="220" height="135" />');
+});
+
+// Use port 3000 unless an environment variable (PORT) is set
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on "http://' +
+  os.hostname() +
+  ':'
+  + port + 
+  '" ');
+});
